16387abb39f0759c4c155a376d6410e094bacb54
Leonardo Miguel <leonardomdubena@gmail.com>
2024-09-23 17:25:43 -0300

    JS

---
+++ js/codigos.js
@@ -0,0 +1,15 @@
+// let nome = prompt("Informe seu nome: ");
+// console.log("Foi digitado: "+ nome);
+
+let a = 10;
+let b = 5;
+console.log(a+b);
+
+const PI=3.1416;
+
+function copia_cola(){
+    let n = document.getElementById('nome').value;
+    let resultado = "<li>" + n + "</li>"
+    document.getElementById('contato_img').innerHTML += resultado; 
+    console.log(n);
+}
